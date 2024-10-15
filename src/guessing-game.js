15c27dc90db85691cc63e6b482045345bcd71544
joelans <baileyderek59@gmail.com>
2024-10-15 23:56:41 +0300

    feat: solve guessing game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.i = 0;
+    this.mid = 0;
+    this.j = 0;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.i = min;
+    this.j = max;
+  }
 
-    }
+  guess() {
+    this.mid = this.i + Math.ceil((this.j - this.i) / 2);
+    return this.mid;
+  }
 
-    guess() {
+  lower() {
+    this.j = this.mid;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.i = this.mid;
+  }
 }
 
 module.exports = GuessingGame;
